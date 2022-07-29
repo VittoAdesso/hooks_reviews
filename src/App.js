deb93a0014728f73bf9ef3c3c8449caa8e0d2ce1
Vittorio Adesso <adessovittorio8@gmail.com>
2022-07-29 21:20:45 +0200

    useState fase 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import ShowContent from './hookUseState/showContent';
 
 function App() {
   return (
     <div className="App">
-    
+      <ShowContent message="Vittoooo, you got It!"/>
     </div>
   );
 }
