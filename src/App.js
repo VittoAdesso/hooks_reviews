aadb143f5538d9df6961dbf8b7739a103ba9f1e5
Vittorio Adesso <adessovittorio8@gmail.com>
2022-07-29 21:44:01 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import ShowContent from './hookUseState/showContent';
+import BeerTime from './hookUseState/beerTime';
 
 function App() {
   return (
+    // TODO: routers || links into a nav bar 
     <div className="App">
       <ShowContent message="Vittoooo, you got It!"/>
+      <hr/>
+      <BeerTime message="Beer Time my Friend"/>
+      <hr/>
     </div>
   );
 }
